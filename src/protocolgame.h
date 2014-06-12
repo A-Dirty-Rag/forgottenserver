6fda59b1615d5f1065fc4e8c5657e76c38e3b8ab
A-Dirty-Rag <odorinotsuki@hotmail.com>
2014-06-12 06:39:47 -0700

    Update protocolgame.h
    breaking battle

--- src/protocolgame.h
+++ src/protocolgame.h
@@ -118,7 +118,6 @@ class ProtocolGame : public Protocol
 		void parseSetOutfit(NetworkMessage& msg);
 		void parseSay(NetworkMessage& msg);
 		void parseLookAt(NetworkMessage& msg);
-		void parseLookInBattleList(NetworkMessage& msg);
 		void parseFightModes(NetworkMessage& msg);
 		void parseAttack(NetworkMessage& msg);
 		void parseFollow(NetworkMessage& msg);
